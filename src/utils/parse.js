7ad64f6c62ede9ab9cdd83d70ff1deb73011d5ec
bbbbmo <aqw20501@naver.com>
2025-11-03 20:56:22 +0900

    remove: 사용하지 않는 parse.js 삭제

--- src/utils/parse.js
+++
@@ -1,6 +0,0 @@
-import { Console } from "@woowacourse/mission-utils";
-
-export const parseInput = async (field) => {
-  const rawInput = await Console.readLineAsync(`${field}을 입력해 주세요.\n`);
-  return rawInput.trim();
-};
